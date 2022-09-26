6c1058cfbac840639d446a6e8e2d85956677f850
DavidAshburn <oneinchwebbing@gmail.com>
2022-09-26 01:01:22 -0700

    initial commit, devise and tailwind configured

---
+++ config/tailwind.config.js
@@ -0,0 +1,33 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      backgroundSize: {
+        'oversize': '300%',
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        'raleway': ['Raleway', 'sans-serif'],
+        'monoton': ['Monoton', 'cursive'],
+        'righteous': ['Righteous', 'cursive'],
+        'questrial': ['Questrial', 'sans-serif'],
+        'lato': ['Lato', 'sans-serif'],
+        'bebas': ['Bebas Neue', 'cursive'],
+        'ptmono': ['PT Mono', 'monospace'],
+        'urbanist': ['Urbanist', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ]
+}
